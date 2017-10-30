7b820a4ffd26e2e49d942ed77f5a0f02b7528b9c
afaraldo <iceberg.04@gmail.com>
2017-10-29 23:43:37 -0300

    add item superclass

--- isp-platform/src/com/sd/isp/domain/part/PartDomain.java
+++ isp-platform/src/com/sd/isp/domain/part/PartDomain.java
@@ -7,67 +7,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import com.sd.isp.domain.base.BaseDomain;
+import com.sd.isp.domain.item.ItemDomain;
 
 @Entity
-@Table(name = "part")
-public class PartDomain extends BaseDomain {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "id", nullable = false, unique = true)
-	private Integer id;
-
-	@Column(name = "name")
-	private String name;
-
-	@Column(name = "description", nullable = true)
-	private String description;
-
-	@Column(name = "price")
-	private Integer price;
-
-	@Column(name = "quantity", nullable = true)
-	private Integer quantity;
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public Integer getPrice() {
-		return price;
-	}
-
-	public void setPrice(Integer price) {
-		this.price = price;
-	}
-
-	public Integer getQuantity() {
-		return quantity;
-	}
-
-	public void setQuantity(Integer quantity) {
-		this.quantity = quantity;
-	}
-
+public class PartDomain extends ItemDomain {
 }
--- isp-platform/src/import.sql
+++ isp-platform/src/import.sql
@@ -4,15 +4,15 @@ insert into cars (color, mark, model, number) values ('azul','Audi','deportivo',
 insert into cars (color, mark, model, number) values ('gris','Mercedes Benz','deportivo', 124);
 insert into cars (color, mark, model, number) values ('negro','BMW','deportivo', 125);
 
-insert into person (DTYPE,name, surname, RUC, address, cellphone, personType) values ('client','adrian','aguero','4925875', 'Centro', '0985124789', 'Proveedor');
-insert into person (DTYPE,name, surname, RUC, address, cellphone, personType) values ('client','karina','sanabria','123456', 'Centro', '0875412369', 'Proveedor');
-insert into person (DTYPE,name, surname, RUC, address, cellphone, personType) values ('client','guillermo','zarza','123457', 'Centro', '0995123478', 'Cliente');
-insert into person (DTYPE,name, surname, RUC, address, cellphone, personType) values ('client','lorena','delpuerto','1234568', 'Centro', '0985012369', 'Empleado');
-insert into person (DTYPE,name, surname, RUC, address, cellphone, personType) values ('client','Albert','Einstein','123459', 'Centro', '0985102369', 'Proveedor');
-insert into person (DTYPE,name, surname, RUC, address, cellphone, personType) values ('client','Marie','Curie','1234560', 'Centro', '0994125786', 'Cliente');
+insert into person (DTYPE, name, surname, RUC, address, cellphone, personType) values ('ClientDomain','adrian','aguero','4925875', 'Centro', '0985124789', 'Proveedor');
+insert into person (DTYPE, name, surname, RUC, address, cellphone, personType) values ('SupplierDomain','karina','sanabria','123456', 'Centro', '0875412369', 'Proveedor');
+insert into person (DTYPE, name, surname, RUC, address, cellphone, personType) values ('ClientDomain','guillermo','zarza','123457', 'Centro', '0995123478', 'Cliente');
+insert into person (DTYPE, name, surname, RUC, address, cellphone, personType) values ('SupplierDomain','lorena','delpuerto','1234568', 'Centro', '0985012369', 'Empleado');
+insert into person (DTYPE, name, surname, RUC, address, cellphone, personType) values ('EmployeeDomain','Albert','Einstein','123459', 'Centro', '0985102369', 'Proveedor');
+insert into person (DTYPE, name, surname, RUC, address, cellphone, personType) values ('EmployeeDomain','Marie','Curie','1234560', 'Centro', '0994125786', 'Cliente');
 
-insert into part (name, price, quantity) values ('cambio de aceite','100000',	null);
-insert into part (name, price, quantity) values ('cambio de frenos','250000',	null);
-insert into part (name, price, quantity) values ('caja mecanica',	'500000',	1);
-insert into part (name, price, quantity) values ('cambio de motor',	'4000000',	null);
-insert into part (name, price, quantity) values ('motor',			'2000000',	4);
+insert into items (DTYPE, name, price, quantity) values ('ServiceDomain', 'cambio de aceite','100000',	null);
+insert into items (DTYPE, name, price, quantity) values ('ServiceDomain', 'cambio de frenos','250000',	null);
+insert into items (DTYPE, name, price, quantity) values ('PartDomain', 'caja mecanica',	'500000',	1);
+insert into items (DTYPE, name, price, quantity) values ('ServiceDomain', 'cambio de motor',	'4000000',	null);
+insert into items (DTYPE, name, price, quantity) values ('PartDomain', 'motor',			'2000000',	4);
