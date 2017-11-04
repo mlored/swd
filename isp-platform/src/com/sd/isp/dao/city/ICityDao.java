5dc154c7a66e0de25db4d50281b9a4a40072a148
afaraldo <iceberg.04@gmail.com>
2017-11-04 16:16:41 -0300

    remove City, State, Country in all layers

--- isp-platform/src/com/sd/isp/dao/city/ICityDao.java
+++
@@ -1,8 +0,0 @@
-package com.sd.isp.dao.city;
-
-import com.sd.isp.dao.base.IBaseDao;
-import com.sd.isp.domain.location.city.CityDomain;
-
-public interface ICityDao extends IBaseDao<CityDomain> {
-
-}
--- isp-platform/src/com/sd/isp/dao/state/IStateDao.java
+++
@@ -1,8 +0,0 @@
-package com.sd.isp.dao.state;
-
-import com.sd.isp.dao.base.IBaseDao;
-import com.sd.isp.domain.location.state.StateDomain;
-
-public interface IStateDao extends IBaseDao<StateDomain> {
-
-}
