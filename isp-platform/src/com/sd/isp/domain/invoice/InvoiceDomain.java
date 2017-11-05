f4f4b4d0e3d822d465683f6e26f40ef849b9f8c4
afaraldo <iceberg.04@gmail.com>
2017-11-05 17:36:11 -0300

    Fix InvoiceDomain

--- isp-platform/src/com/sd/isp/domain/invoice/InvoiceDomain.java
+++ isp-platform/src/com/sd/isp/domain/invoice/InvoiceDomain.java
@@ -1,14 +1,17 @@
 package com.sd.isp.domain.invoice;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import com.sd.isp.domain.base.BaseDomain;
+import com.sd.isp.domain.invoice_details.InvoiceDetailsDomain;
 
 @Entity
 @Table(name = "invoice")
@@ -31,8 +34,8 @@ public class InvoiceDomain extends BaseDomain {
 	@Column(name = "total")
 	private Integer total;
 	
-	/*@OneToMany(mappedBy = "invoiceDomain")
-    private List<InvoiceDetailsDomain> invoiceDetailsDomains;*/
+	@OneToMany(mappedBy = "invoiceDomain")
+    private List<InvoiceDetailsDomain> invoiceDetailsDomains;
 	
 	public Integer getId() {
 		return id;
