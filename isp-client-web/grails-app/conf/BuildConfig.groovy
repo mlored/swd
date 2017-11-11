ee7af841467ef18f5dcfd88fce65184ce4f69bfb
afaraldo <iceberg.04@gmail.com>
2017-11-11 19:59:24 -0300

    add admin-lte theme

--- isp-client-web/grails-app/conf/BuildConfig.groovy
+++ isp-client-web/grails-app/conf/BuildConfig.groovy
@@ -61,13 +61,14 @@ grails.project.dependency.resolution = {
 		// plugins for the compile step
 		compile ":scaffolding:2.1.2"
 		compile ':cache:1.1.7'
-		compile ":asset-pipeline:1.9.6"
+		compile ":asset-pipeline:2.13.1"
 
 		// plugins needed at runtime but not for compilation
 		runtime ":hibernate4:4.3.5.5" // or ":hibernate:3.6.10.17"
 		runtime ":database-migration:1.4.0"
 		runtime ":jquery:1.11.1"
 		runtime ':twitter-bootstrap:3.3.5' // current: 3.3.5
+		runtime ':adminlte-ui:0.1.0'
 
 		// Uncomment these to enable additional asset-pipeline capabilities
 		//compile ":sass-asset-pipeline:1.9.0"
