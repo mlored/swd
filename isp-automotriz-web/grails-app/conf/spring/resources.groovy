7772f6a3a1b67b3e970fddedafeb4c9215f6b2eb
mlored <loredelpuerto@gmail.com>
2018-11-25 23:18:01 -0300

    add resource and service for beans spring

--- isp-automotriz-web/grails-app/conf/spring/resources.groovy
+++ isp-automotriz-web/grails-app/conf/spring/resources.groovy
@@ -1,3 +1,29 @@
+import com.sd.isp.rest.car.CarResourceImpl
+import com.sd.isp.service.car.CarServiceImpl
+import com.sd.isp.rest.client.ClientResourceImpl
+import com.sd.isp.service.client.ClientServiceImpl
+import com.sd.isp.rest.employee.EmployeeResourceImpl
+import com.sd.isp.service.employee.EmployeeServiceImpl
+import com.sd.isp.rest.part.PartResourceImpl
+import com.sd.isp.service.part.PartServiceImpl
+import com.sd.isp.rest.service.ServiceResourceImpl
+import com.sd.isp.service.service.ServiceServiceImpl
+
 // Place your Spring DSL code here
 beans = {
+    //resources
+    carResource(CarResourceImpl)
+    clientResource(ClientResourceImpl)
+    employeeResource(EmployeeResourceImpl)
+    partResource(PartResourceImpl)
+    serviceResource(ServiceResourceImpl)
+
+    //services
+    carService(CarServiceImpl)
+    clientService(ClientServiceImpl)
+    employeeService(EmployeeServiceImpl)
+    partService(PartServiceImpl)
+    serviceResource(ServiceServiceImpl)
+
+
 }
