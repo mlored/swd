7122609acc418d2a6c18768d5ca82017adc1ae51
mlored <loredelpuerto@gmail.com>
2018-12-07 20:04:29 -0300

    add spring security config

--- taller-automotriz-web/grails-app/conf/spring/resources.groovy
+++ taller-automotriz-web/grails-app/conf/spring/resources.groovy
@@ -1,4 +1,6 @@
+import com.sd.isp.secure.UserPasswordEncoderListener
 // Place your Spring DSL code here
 beans = {
+    userPasswordEncoderListener(UserPasswordEncoderListener, ref('hibernateDatastore'))
 
 }
